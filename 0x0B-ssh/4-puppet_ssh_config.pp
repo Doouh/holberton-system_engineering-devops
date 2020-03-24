025f9ea69dd6b60a91166b0096217fcff3609751
Doouh <ake.giraldo@hotmail.com>
2020-03-23 23:30:08 -0500

    Fix task 4

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,8 +1,8 @@
 # Set up your client SSH configuration file so that you can connect to a server without typing a password
-file_line { "Turn off passwd auth":
-  path  => "/etc/ssh/ssh_config",
-  line  => "set PasswordAuthentication no",
-  match => "PasswordAuthentication",
+file_line { 'Turn off passwd auth':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'set PasswordAuthentication no',
+  match => 'PasswordAuthentication',
 }
 
 file_line { 'Declare identity file':
