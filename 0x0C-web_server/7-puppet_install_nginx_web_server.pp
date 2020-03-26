93b0649ea510d361fa5b11409e6a33a0b9a2dcff
Doouh <ake.giraldo@hotmail.com>
2020-03-25 19:26:42 -0500

    Fix task 7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,25 +1,13 @@
 #Puppet manifest containing commands to automatically configure an Ubuntu machine to respect above requirements
-exec { 'update_packages':
+exec { 'update_install':
   path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command  => 'apt-get update',
+  command  => 'apt-get -y update && apt-get -y install nginx',
   provider => 'shell'
 }
 
-exec { 'install_nginx':
+exec { 'modify_files':
   path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command  => 'apt-get -y install nginx',
-  provider => 'shell'
-}
-
-exec { 'create_index':
-  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command  => "echo -e 'Holberton School' > /var/www/html/index.html",
-  provider => 'shell'
-}
-
-exec { 'config_redirect':
-  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command  => "sed -i '/listen 80 default_server;/a rewrite ^/redirect_me https://www.linkedin.com/ permanent;' /etc/nginx/sites-available/default",
+  command  => "echo 'Holberton School' > /var/www/html/index.html && sed -i '/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/ permanent;' /etc/nginx/sites-available/default",
   provider => 'shell'
 }
 
