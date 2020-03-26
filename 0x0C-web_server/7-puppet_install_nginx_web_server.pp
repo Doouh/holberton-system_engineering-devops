4229f22b7565d57385443bfa610076e3d4e80b20
Doouh <ake.giraldo@hotmail.com>
2020-03-25 19:14:12 -0500

    Fix task 7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,25 +1,30 @@
 #Puppet manifest containing commands to automatically configure an Ubuntu machine to respect above requirements
 exec { 'update_packages':
-  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command => 'apt-get update'
+  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command  => 'apt-get update',
+  provider => 'shell'
 }
 
 exec { 'install_nginx':
-  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command => 'apt-get -y install nginx'
+  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command  => 'apt-get -y install nginx',
+  provider => 'shell'
 }
 
 exec { 'create_index':
-  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command => "echo -e 'Holberton School' > /var/www/html/index.html"
+  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command  => "echo -e 'Holberton School' > /var/www/html/index.html",
+  provider => 'shell'
 }
 
 exec { 'config_redirect':
-  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command => "sed -i '/listen 80 default_server;/a rewrite ^/redirect_me https://www.linkedin.com/ permanent;' /etc/nginx/sites-available/default"
+  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command  => "sed -i '/listen 80 default_server;/a rewrite ^/redirect_me https://www.linkedin.com/ permanent;' /etc/nginx/sites-available/default",
+  provider => 'shell'
 }
 
 exec { 'start_nginx':
-  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command => 'service nginx start'
+  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command  => 'service nginx start',
+  provider => 'shell'
 }
