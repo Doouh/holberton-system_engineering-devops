9081a06c5bdf7395519d271a7c795feb0e279ce5
Doouh <ake.giraldo@hotmail.com>
2020-03-25 19:59:10 -0500

    Fix task 7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,15 +1,5 @@
 #Puppet manifest containing commands to automatically configure an Ubuntu machine to respect above requirements
-exec { 'update_install':
-  command  => 'apt-get -y update && apt-get -y install nginx',
-  provider => 'shell'
-}
-
-exec { 'modify_files':
-  command  => "echo 'Holberton School' > /var/www/html/index.html && sed -i '/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/ permanent;' /etc/nginx/sites-available/default",
-  provider => 'shell'
-}
-
-exec { 'start_nginx':
-  command  => 'service nginx start',
-  provider => 'shell'
+exec { 'install_configure_nginx':
+  command  => "apt-get -y update && apt-get -y install nginx && echo 'Holberton School' > /var/www/html/index.html && sed -i '/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/ permanent;' /etc/nginx/sites-available/default && service nginx start",
+  provider => 'shell',
 }
