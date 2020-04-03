b5d2b2ccc5591d8dbec4b484d23ca93ac4649488
Doouh <ake.giraldo@hotmail.com>
2020-04-03 13:17:42 -0500

    Add task 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,5 @@
+#Puppet manifest containing commands to automatically configure an Ubuntu machine to respect above requirements
+exec { 'install_configure_nginx':
+  command  => "apt-get -y update && apt-get -y install nginx && echo 'Holberton School' > /var/www/html/index.html && sed -i '/sendfile on;/a add_header X-Served-By $HOSTNAME;' /etc/nginx/nginx.conf && service nginx start",
+  provider => 'shell',
+}
