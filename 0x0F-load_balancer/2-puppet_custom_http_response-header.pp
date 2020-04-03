66a9b87b258b732c5ea4a927b98b304654401824
Doouh <ake.giraldo@hotmail.com>
2020-04-03 14:24:56 -0500

    Fix task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,5 +1,5 @@
 #Puppet manifest containing commands to automatically configure an Ubuntu machine to respect above requirements
 exec { 'install_nginx_add_header':
-  command  => "sed -i '/sendfile on;/a add_header X-Served-By {$HOSTNAME};' /etc/nginx/nginx.conf",
+  command  => "apt-get update && apt-get -y install nginx && echo -e 'Holberton School' > /var/www/html/index.html && sed -i '/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/ permanent;' /etc/nginx/sites-available/default && sed -i '22i add_header X-Served-By $hostname;' /etc/nginx/nginx.conf && service nginx start",
   provider => 'shell',
 }
