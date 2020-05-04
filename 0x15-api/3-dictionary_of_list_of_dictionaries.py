a828e4595258d4b7f7341da947c1003ce07848d8
Doouh <ake.giraldo@hotmail.com>
2020-05-04 17:25:21 -0500

    Add task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Script to export data in the JSON format.
+"""
+
+import json
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/users/'
+    users = requests.get(url).json()
+    allTasks = {}
+    for user in users:
+        url = 'https://jsonplaceholder.typicode.com/todos?UserId=' +\
+               str(user['id'])
+        tasks = requests.get(url).json()
+        ls = []
+        for task in tasks:
+            dic = {}
+            dic['task'] = task['title']
+            dic['completed'] = task['completed']
+            dic['username'] = user['username']
+            ls.append(dic)
+        allTasks[user['id']] = ls
+
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(allTasks, f)
