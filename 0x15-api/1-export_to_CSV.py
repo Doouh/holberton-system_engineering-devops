24d4e6f9ee8d8a9ac6b8e57c159e4cb32fb83946
Doouh <ake.giraldo@hotmail.com>
2020-05-04 16:00:53 -0500

    Delete try

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,16 +8,13 @@ from sys import argv
 import csv
 
 if __name__ == '__main__':
-    try:
-        url = 'https://jsonplaceholder.typicode.com/users/' + argv[1]
-        username = requests.get(url).json()['username']
-        url = 'https://jsonplaceholder.typicode.com/todos?userId=' + argv[1]
-        tasks = requests.get(url).json()
-        with open(argv[1]+'.csv', 'w', newline='') as File:
-            writer = csv.writer(File, delimiter=',',
-                                quotechar='"', quoting=csv.QUOTE_ALL)
-            for task in tasks:
-                writer.writerow([argv[1]] + [username] +
-                                [task['completed']] + [task['title']])
-    except Exception:
-        pass
+    url = 'https://jsonplaceholder.typicode.com/users/' + argv[1]
+    username = requests.get(url).json()['username']
+    url = 'https://jsonplaceholder.typicode.com/todos?userId=' + argv[1]
+    tasks = requests.get(url).json()
+    with open(argv[1]+'.csv', 'w', newline='') as File:
+        writer = csv.writer(File, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_ALL)
+        for task in tasks:
+            writer.writerow([argv[1]] + [username] +
+                            [task['completed']] + [task['title']])
