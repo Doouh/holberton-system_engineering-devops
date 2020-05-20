5f0a6f2890a810b14e46baf7ad082ce14090a704
Doouh <ake.giraldo@hotmail.com>
2020-05-20 09:27:07 -0500

    Restart apache

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
 exec { 'fixed_extension_file':
-    command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+    command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php && service apache2 reload",
     path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
 }
