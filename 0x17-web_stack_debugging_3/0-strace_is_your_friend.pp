589bb8ea1abb4eb60d9e42a847ca39e859b42856
Doouh <ake.giraldo@hotmail.com>
2020-05-20 09:29:03 -0500

    Add comment

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+#Change extension from phpp to php
 exec { 'fixed_extension_file':
     command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php && service apache2 reload",
     path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
